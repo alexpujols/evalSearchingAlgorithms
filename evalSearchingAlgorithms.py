0ab38d2df324aca96da062f0864eff5619942e4a
Alex <alex.pujols@gmail.com>
2021-02-06 09:36:39 -0600

    New header information

--- evalSearchingAlgorithms.py
+++ evalSearchingAlgorithms.py
@@ -1,12 +1,33 @@
 ##!/usr/bin/env python
+'''
+                      ::::::
+                    :+:  :+:
+                   +:+   +:+
+                  +#++:++#++:::::::
+                 +#+     +#+     :+:
+                #+#      #+#     +:+
+               ###       ###+:++#""
+                         +#+
+                         #+#
+                         ###
+'''
 __author__ = "Alex Pujols"
-__copyright__ = "Copyright 2021, Evaluating Searching Algorithms Project TIM-6110 wk6"
+__copyright__ = "TBD"
 __credits__ = ["Alex Pujols"]
 __license__ = "GPL"
 __version__ = "1.0"
 __maintainer__ = "Alex Pujols"
 __email__ = "alex.pujols@gmail.com"
 __status__ = "Prototype"
+
+'''
+Title	      :	{TBD}
+Date		  :	{XX-XX-20XX}
+Description   :	{TBD}
+Options	      :	{TBD}
+Notes	      :	{TBD}
+'''
+
 # Import modules declarations
 from random import randint
 from collections import Counter
